d884f84932df8c0339f4022b4a29586c94b34d46
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-09-13 20:02:44 +1000

    Add comments and ToString

--- RayTracer/Objects/Plane.cs
+++ RayTracer/Objects/Plane.cs
@@ -5,15 +5,56 @@ using System.Text;
 
 namespace RayTracer.Objects
 {
-    public class Plane : GeometricObject
+    /// <summary>
+    /// A 3D flat, infinite plane.
+    /// </summary>
+    public class Plane : GeometricObject, IEquatable<Plane?>
     {
-        public Plane(Point3D point, Material material, Vector3D normal)
-            : base(point, material)
+        /// <summary>
+        /// Create a Plane.
+        /// </summary>
+        /// <param name="location">
+        /// A point on the plane.
+        /// </param>
+        /// <param name="material">
+        /// The material used to colour or shade the plane.
+        /// </param>
+        /// <param name="normal">
+        /// A normal to the plane, specifying its orientation at <paramref name="location"/>.
+        /// </param>
+        public Plane(Point3D location, Material material, Vector3D normal)
+            : base(location, material)
         {
             Normal = normal;
         }
 
+        /// <summary>
+        /// A normal to the plane, specifying its orientation at <see cref="Location"/>.
+        /// </summary>
         public Vector3D Normal { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Plane);
+        }
+
+        public bool Equals(Plane? other)
+        {
+            return other != null &&
+                   Location.Equals(other.Location) &&
+                   Material.Equals(other.Material) &&
+                   Normal.Equals(other.Normal);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Location, Material, Normal);
+        }
+
+        public override string ToString()
+        {
+            return $"Point { Location } Normal { Normal }";
+        }
 
         /// <inheritdoc/>
         public override bool Hit(Ray ray, out double distance, out ShadeRecord? shadeRecord)
--- RayTracer/Ray.cs
+++ RayTracer/Ray.cs
@@ -5,16 +5,34 @@ using RayTracer.Primitives;
 
 namespace RayTracer
 {
+    /// <summary>
+    /// A ray, essentially a vector from a point.
+    /// </summary>
     public class Ray : IEquatable<Ray?>
     {
+        /// <summary>
+        /// Create a Ray.
+        /// </summary>
+        /// <param name="origin">
+        /// The point the ray starts from.
+        /// </param>
+        /// <param name="direction">
+        /// The ray's direction.
+        /// </param>
         public Ray(Point3D origin, Vector3D direction)
         {
             Origin = origin;
             Direction = direction;
         }
 
+        /// <summary>
+        /// The point the ray starts from.
+        /// </summary>
         public Point3D Origin { get; }
 
+        /// <summary>
+        /// The ray's direction.
+        /// </summary>
         public Vector3D Direction { get; }
 
         public override bool Equals(object? obj)
@@ -42,6 +60,11 @@ namespace RayTracer
         public static bool operator !=(Ray? left, Ray? right)
         {
             return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"Origin { Origin }, Direction { Direction }";
         }
     }
 }
