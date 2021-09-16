377a6ea8daf8dffa125e5b32311a8c48bc6a2332
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-09-16 18:16:18 +1000

    Add ConvertColor test

---
+++ RayTracer.Test/TestImageSerializer.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Test
+{
+    internal class TestImageSerializer
+    {
+        [TestCase(0, 0, 0, 0, 0, 0)]
+        [TestCase(0.5, 0, 0, 128, 0, 0)]
+        [TestCase(0, 0.5, 0, 0, 128, 0)]
+        [TestCase(0, 0, 0.5, 0, 0, 128)]
+        [TestCase(1.0, 1.0, 1.0, 255, 255, 255)]
+        public void TestConvertColor(double red, double green, double blue, byte expectedRed, byte expectedGreen, byte expectedBlue)
+        {
+            Assert.That(
+                new ImageSerializer().ConvertColor(new RGBColor(red, green, blue)),
+                Is.EqualTo(Color.FromArgb(expectedRed, expectedGreen, expectedBlue)));
+        }
+    }
+}
--- RayTracer/ImageSerializer.cs
+++ RayTracer/ImageSerializer.cs
@@ -8,8 +8,20 @@ using System.Drawing.Imaging;
 
 namespace RayTracer
 {
-    internal class ImageSerializer
+    /// <summary>
+    /// Save an RGBColor array as a PNG.
+    /// </summary>
+    public class ImageSerializer
     {
+        /// <summary>
+        /// Save an RGBColor array as a PNG.
+        /// </summary>
+        /// <param name="output">
+        /// The ray tracer output.
+        /// </param>
+        /// <param name="fileName">
+        /// The output file name.
+        /// </param>
         public void Save(RGBColor[,] output, string fileName)
         {
             Bitmap bitmap = new Bitmap(output.GetLength(0), output.GetLength(1));
@@ -24,12 +36,26 @@ namespace RayTracer
             bitmap.Save(Path.ChangeExtension(fileName, "png"), ImageFormat.Png);
         }
 
+        /// <summary>
+        /// Convert an <see cref="RGBColor"/> to <see cref="Color"/>.
+        /// </summary>
+        /// <param name="rgbColor">
+        /// The <see cref="RGBColor"/> to convert.
+        /// </param>
+        /// <returns>
+        /// The converted <see cref="Color"/>.
+        /// </returns>
+        /// <exception cref="OverflowException">
+        /// The <see cref="RGBColor.Red"/>, <see cref="RGBColor.Green"/> or <see cref="RGBColor.Blue"/> values
+        /// must be between 0 to 1 inclusive.
+        /// </exception>
         public Color ConvertColor(RGBColor rgbColor)
         {
+            // TODO: Consider gamma. 
             return Color.FromArgb(
-                Convert.ToByte(rgbColor.Red * 256),
-                Convert.ToByte(rgbColor.Green * 256),
-                Convert.ToByte(rgbColor.Blue * 256));
+                Convert.ToByte(rgbColor.Red * Byte.MaxValue),
+                Convert.ToByte(rgbColor.Green * Byte.MaxValue),
+                Convert.ToByte(rgbColor.Blue * Byte.MaxValue));
         }
     }
 }
