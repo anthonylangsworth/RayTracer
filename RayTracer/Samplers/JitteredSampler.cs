7792f06faf5c651fef4d56e736e85529b31414a5
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-09-21 17:47:41 +1000

    Enforce sqaure samples per set

--- RayTracer/Samplers/JitteredSampler.cs
+++ RayTracer/Samplers/JitteredSampler.cs
@@ -16,8 +16,8 @@ namespace RayTracer.Samplers
         /// The random number generator to use.
         /// </param>
         /// <param name="samplesPerSet">
-        /// The number of samples per set. This should be a square 
-        /// number (e.g. 4, 9, 16, 25, 36, ...) and must be positive.
+        /// The number of samples per set. This must be a positive square 
+        /// number (e.g. 4, 9, 16, 25, 36, ...).
         /// </param>
         /// <param name="sampleSets">
         /// The number of sample sets, which defaults to 1. This must
@@ -29,15 +29,19 @@ namespace RayTracer.Samplers
         public JitteredSampler(Random random, int samplesPerSet, int sampleSets = 1) 
             : base(random, samplesPerSet, sampleSets)
         {
-            // Do nothing
+            int samplesPerSetSquareRoot = (int) Math.Sqrt(samplesPerSet);
+            if (samplesPerSetSquareRoot * samplesPerSetSquareRoot != samplesPerSet)
+            {
+                throw new ArgumentOutOfRangeException(nameof(samplesPerSet), 
+                    $"{ nameof(samplesPerSet) } must be a square number");
+            }
         }
 
         /// <inheritdoc/>
         protected override IEnumerable<Point2D> GenerateSample(Random random)
         {
-            int sampleMax = (int) Math.Sqrt(SamplesPerSet);
             Point2D[] result = new Point2D[SamplesPerSet];
-
+            int sampleMax = (int)Math.Sqrt(SamplesPerSet);
             for (int sampleRow = 0; sampleRow < sampleMax; sampleRow++) // up
             {
                 for (int sampleColumn = 0; sampleColumn < sampleMax; sampleColumn++) // left to right
