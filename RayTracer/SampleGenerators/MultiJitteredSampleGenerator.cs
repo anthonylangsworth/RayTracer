b986b302cebcdd552c73cb54aae353907c9b1139
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-10-18 12:10:02 +1100

    Add single pass shuffle

--- RayTracer/SampleGenerators/MultiJitteredSampleGenerator.cs
+++ RayTracer/SampleGenerators/MultiJitteredSampleGenerator.cs
@@ -46,8 +46,9 @@ namespace RayTracer.SampleGenerators
             //    }
             //}
 
-            result = ShuffleXValuesInRows(random, result);
-            result = ShuffleYValuesInColumns(random, result);
+            //result = ShuffleXValuesInRows(random, result);
+            //result = ShuffleYValuesInColumns(random, result);
+            result = ShufflePoints(random, result);
             return result.Cast<Point2D>();
         }
 
@@ -66,6 +67,32 @@ namespace RayTracer.SampleGenerators
                     );
                 }
             }
+            return result;
+        }
+
+        public static Point2D[,] ShufflePoints(Random random, Point2D[,] points)
+        {
+            Point2D[,] result = new Point2D[points.GetLength(0), points.GetLength(1)];
+            double[][] newXs = new double[points.GetLength(0)][]; // Row first, like points
+            double[][] newYs = new double[points.GetLength(1)][]; // Column first
+
+            for (int row = 0; row < points.GetLength(0); row++)
+            {
+                newXs[row] = Enumerable.Range(0, points.GetLength(1)).Shuffle(random).Select(column => points[row, column].X).ToArray();
+            }
+            for (int column = 0; column < points.GetLength(1); column++)
+            {
+                newYs[column] = Enumerable.Range(0, points.GetLength(0)).Shuffle(random).Select(row => points[row, column].Y).ToArray();
+            }
+
+            for (int sampleRow = 0; sampleRow < points.GetLength(0); sampleRow++)
+            {
+                for (int sampleColumn = 0; sampleColumn < points.GetLength(1); sampleColumn++)
+                {
+                    result[sampleRow, sampleColumn] = new Point2D(newXs[sampleRow][sampleColumn], newYs[sampleColumn][sampleRow]);
+                }
+            }
+
             return result;
         }
 
