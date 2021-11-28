459d6d0cbabbcf8e75ad119521b03f676fb3ccdf
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-11-29 00:42:23 +1100

    Add initial hemisphere mapping

---
+++ RayTracer/SampleGenerators/HemisphereSampleMapping.cs
@@ -0,0 +1,32 @@
+﻿using RayTracer.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.SampleGenerators
+{
+    internal class HemisphereSampleMapping
+    {
+        public HemisphereSampleMapping(double e)
+        {
+            E = e;
+        }
+
+        public double E { get; }
+
+        public Point3D Map(Point2D point)
+        {
+            double cosPhi = Math.Cos(2 * Math.PI * point.X);
+            double sinPhi = Math.Sin(2 * Math.PI * point.X);
+            double cosTheta = Math.Pow(1 - point.Y, 1 / (E + 1));
+            double sinTheta = Math.Sqrt(1 - cosTheta * cosTheta);
+            double pu = sinTheta * cosPhi;
+            double pv = sinTheta * sinPhi;
+            double pw = cosTheta;
+
+            return new Point3D(pu, pv, pw);
+        }
+    }
+}
