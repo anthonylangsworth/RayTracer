d01194b8d1ea684aff1271a94d67f1f58de0dbea
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-12-02 15:09:55 +1100

    Add UnitDiskMapper test

---
+++ RayTracer.Test/SampleGenerators/TestUnitDiskSampleMapper.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using RayTracer.Primitives;
+using RayTracer.SampleGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Test.SampleGenerators
+{
+    internal class TestUnitDiskSampleMapper
+    {
+        [Test]
+        [Repeat(10)]
+        public void Map()
+        {
+            IEnumerable<Point2D> mappedPoints = SampleAlgorithms.Hammersley.GenerateSampleSet(new Random(), 64).Select(point => SampleMappers.UnitDisk.Map(point));
+            Assert.That(mappedPoints.Select(point => Math.Sqrt(point.X * point.X + point.Y * point.Y)), Is.All.LessThanOrEqualTo(1));
+        }
+    }
+}
