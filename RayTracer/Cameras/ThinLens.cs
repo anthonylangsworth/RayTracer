69eac67a1e42cc075042302084992f1a2d12a35f
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-12-05 23:15:55 +1100

    Start ThinLens camera

---
+++ RayTracer/Cameras/ThinLens.cs
@@ -0,0 +1,36 @@
+﻿using RayTracer.Primitives;
+using RayTracer.SampleGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Cameras
+{
+    internal class ThinLens : Camera
+    {
+        public ThinLens(Point3D eye, Point3D lookAt, Vector3D up, double lensRadius,
+            double viewPlaneDistance, double focalPlaneDistance, double zoom,
+            SampleGenerator<Point2D> sampleGenerator, double exposureTime = DefaultExposureTime) 
+            : base(eye, lookAt, up, exposureTime)
+        {
+            LensRadius = lensRadius;
+            ViewPlaneDistance = viewPlaneDistance;
+            FocalPlaneDistance = focalPlaneDistance;
+            Zoom = zoom;
+            SampleGenerator = sampleGenerator;
+        }
+
+        public double LensRadius { get; }
+        public double ViewPlaneDistance { get; }
+        public double FocalPlaneDistance { get; }
+        public double Zoom { get; }
+        public SampleGenerator<Point2D> SampleGenerator { get; }
+
+        public override RGBColor[,] Render(World world)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
