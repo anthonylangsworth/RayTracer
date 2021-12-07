656d1c9788ef2cb8d29bc7534fe0efd56bae8df1
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-12-07 20:05:56 +1100

    Start BRDFs

---
+++ src/RayTracer/Reflection/BRDF.cs
@@ -0,0 +1,28 @@
+﻿using RayTracer.Primitives;
+using RayTracer.SampleGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Reflection
+{
+    /// <summary>
+    /// A Bidrectional Reflectance Distribution Function (BRDF).
+    /// </summary>
+    public abstract class BRDF
+    {
+        protected BRDF(SampleGenerator<Point2D> sampleGenerator)
+        {
+            SampleGenerator = sampleGenerator;
+        }
+
+        public SampleGenerator<Point2D> SampleGenerator { get; }
+
+        public abstract RGBColor F(ShadeRecord shadeRecord, Vector3D incoming, Vector3D outgoing);
+        public abstract RGBColor SampleF(ShadeRecord shadeRecord, Vector3D incoming, Vector3D outgoing);
+        public abstract RGBColor Rho(ShadeRecord shadeRecord, Vector3D outgoing);
+
+    }
+}
---
+++ src/RayTracer/Reflection/Lambertian.cs
@@ -0,0 +1,64 @@
+﻿using RayTracer.Primitives;
+using RayTracer.SampleGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Reflection
+{
+    public class Lambertian : BRDF
+    {
+        /// <summary>
+        /// Create a new <see cref="Lambertian"/> BRDF.
+        /// </summary>
+        /// <param name="sampleGenerator">
+        /// 
+        /// </param>
+        /// <param name="diffuseCoefficient">
+        /// 
+        /// </param>
+        /// <param name="diffuseColor">
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// 
+        /// </exception>
+        public Lambertian(SampleGenerator<Point2D> sampleGenerator, double diffuseCoefficient, RGBColor diffuseColor)
+            : base(sampleGenerator)
+        {
+            if(diffuseCoefficient < 0.0 || diffuseCoefficient > 1.0)
+            {
+                throw new ArgumentException($"{ nameof(diffuseCoefficient) } must be between 0 and 1 inclusive", nameof(diffuseCoefficient));
+            }
+
+            DiffuseCoefficient = diffuseCoefficient;
+            DiffuseColor = diffuseColor;
+        }
+
+        /// <summary>
+        /// kd in the book. The about of light reflected.
+        /// </summary>
+        public double DiffuseCoefficient { get; }
+
+        /// <summary>
+        /// cd in the book. Filters the colour reflected.
+        /// </summary>
+        public RGBColor DiffuseColor { get; }
+
+        public override RGBColor F(ShadeRecord shadeRecord, Vector3D incoming, Vector3D outgoing)
+        {
+            return DiffuseCoefficient * DiffuseColor / Math.PI;
+        }
+
+        public override RGBColor Rho(ShadeRecord shadeRecord, Vector3D outgoing)
+        {
+            return DiffuseCoefficient * DiffuseColor;
+        }
+
+        public override RGBColor SampleF(ShadeRecord shadeRecord, Vector3D incoming, Vector3D outgoing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
